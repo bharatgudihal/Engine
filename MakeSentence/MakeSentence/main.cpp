029f067c881819d9019d26f072b58282766a69dc
bharatgudihal <bharat.gudi@gmail.com>
2016-09-23 16:24:20 -0600

    MakeSentence function completed

--- MakeSentence/MakeSentence/main.cpp
+++ MakeSentence/MakeSentence/main.cpp
@@ -2,13 +2,44 @@
 #define _CRTDBG_MAP_ALLOC
 #include <stdlib.h>
 #include <stdio.h>
+#include <conio.h>
 #include <crtdbg.h>
 #else
 #include <stdlib.h>
 #endif // _DEBUG
 
-char * MakeSentence(const char * strings[]) {
-	
+char* MakeSentence(const char * strings[]) {	
+	char * val;	
+	int memorySize=0;
+	int sourceIndex = 0;
+	int secondarySourceIndex = 0;
+	while (strings[sourceIndex] != NULL) {
+		secondarySourceIndex = 0;
+		while (strings[sourceIndex][secondarySourceIndex] != NULL) {
+			secondarySourceIndex++;
+			memorySize++;
+		}
+		sourceIndex++;
+		memorySize++;
+	}
+	val = (char*)malloc(memorySize+1);
+	sourceIndex = 0;
+	secondarySourceIndex = 0;
+	int destinationIndex = 0;
+	while (strings[sourceIndex] != NULL) {
+		secondarySourceIndex = 0;
+		while (strings[sourceIndex][secondarySourceIndex] != NULL) {
+			val[destinationIndex] = strings[sourceIndex][secondarySourceIndex];
+			destinationIndex++;
+			secondarySourceIndex++;
+		}
+		val[destinationIndex] = ' ';
+		destinationIndex++;
+		sourceIndex++;
+	}
+	val[memorySize - 1] = '.';
+	val[memorySize] = '\0';
+	return val;
 }
 void main( int i_argc, char ** i_argl )
 {
@@ -18,14 +49,12 @@ void main( int i_argc, char ** i_argl )
 		"a",
 		"test",
 		NULL
-	} ;
-
+	} ;	
 	char * pSentence = MakeSentence( strings );
 
 	printf( "The Sentence is: %s", pSentence );
-
+	_getch();
 	free( pSentence );
-
 #if defined(_DEBUG)
 	_CrtDumpMemoryLeaks();
 #endif // _DEBUG
