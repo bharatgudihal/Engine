2844a6bf0fcf6141e9204a1b627a03c7830f1567
bharatgudihal <bharat.gudi@gmail.com>
2016-10-03 20:34:26 -0600

    Added free functionality

--- Engine/Memory/HeapManager.h
+++ Engine/Memory/HeapManager.h
@@ -6,31 +6,36 @@
 #define MIN_BLOCK_SIZE 16
 #include <malloc.h>
 class HeapManager {
-	public:
-		HeapManager();
-		void * allocate(size_t size);
-		void free(void * pointer);
+public:
+	HeapManager();
+	void* allocate(size_t size);
+	void free(void * pointer);
 
-	private:
-		struct BlockDescriptor {
-			BlockDescriptor* next;
-			void* base;
-			size_t size;
-		};
-		unsigned char GUARD_BAND_FILL = 0xBB;
-		int availableBlockDescriptorsCount;
-		void* BLOCK;
-		void* BLOCKDESCRIPTOR_POOL;
-		BlockDescriptor* freeBlocksList;
-		BlockDescriptor* assignedBlocksList;
-		BlockDescriptor* uninitializedBlocksList;
-		void* getPointerFromFreeBlocks(size_t size);
-		BlockDescriptor* splitAndReturnBlock(BlockDescriptor* block, void* splitLocation);
-		void initializeBlockDescriptors();
-		BlockDescriptor* getUninitializedBlock();
-		void returnBlockToUninitializedList(BlockDescriptor* i_blockDescriptor);
-		void* padBlockAndReturnPointer(BlockDescriptor* assignedBlock, int i_size);
-		void putBlockInAssignedBlockList(BlockDescriptor* assignedBlock);
-		void removeBlockFromFreeBlocksList(BlockDescriptor* assignedBlock);
+private:
+	struct BlockDescriptor {
+		BlockDescriptor* next;
+		void* base;
+		size_t size;
+		size_t userSize;
+	};
+	unsigned char GUARD_BAND_FILL = 0xBB;
+	unsigned char FREE_BLOCKS_FILL = 0xCD;
+	int availableBlockDescriptorsCount;
+	void* BLOCK;
+	void* BLOCKDESCRIPTOR_POOL;
+	BlockDescriptor* freeBlocksList;
+	BlockDescriptor* assignedBlocksList;
+	BlockDescriptor* uninitializedBlocksList;
+	void* getPointerFromFreeBlocks(size_t size);
+	BlockDescriptor* splitAndReturnBlock(BlockDescriptor* block, void* splitLocation);
+	void initializeBlockDescriptors();
+	BlockDescriptor* getUninitializedBlock();
+	void returnBlockToUninitializedList(BlockDescriptor* i_blockDescriptor);
+	void* padBlockAndReturnPointer(BlockDescriptor* assignedBlock, int i_size);
+	void putBlockInAssignedBlockList(BlockDescriptor* assignedBlock);
+	void removeBlockFromFreeBlocksList(BlockDescriptor* assignedBlock);
+	BlockDescriptor* findBlockForPointer(void* ptr);
+	void checkGuardBands(void* ptr, BlockDescriptor* assignedBlock);
+	void addBlockToFreeBlocksList(BlockDescriptor* assignedBlock);
 };
 #endif // !MEMORY_ALLOCATOR_H
--- Engine/Memory/Private/HeapManager.cpp
+++ Engine/Memory/Private/HeapManager.cpp
@@ -6,9 +6,10 @@ void HeapManager::initializeBlockDescriptors() {
 	startingPointer += BLOCK_SIZE;
 	uninitializedBlocksList = (BlockDescriptor*)startingPointer;
 	BlockDescriptor* head = (BlockDescriptor*)startingPointer;
-	for (int i = 0; i < NUMBER_OF_BLOCKDESCRIPTORS; i++) {		
+	for (int i = 0; i < NUMBER_OF_BLOCKDESCRIPTORS; i++) {
 		head->base = nullptr;
 		head->size = 0;
+		head->userSize = 0;
 		startingPointer += sizeof(BlockDescriptor);
 		if (i != NUMBER_OF_BLOCKDESCRIPTORS - 1) {
 			head->next = (BlockDescriptor*)startingPointer;
@@ -27,23 +28,25 @@ HeapManager::BlockDescriptor* HeapManager::getUninitializedBlock() {
 	return head;
 }
 
-void HeapManager::returnBlockToUninitializedList(BlockDescriptor* i_blockDescriptor) {	
+void HeapManager::returnBlockToUninitializedList(BlockDescriptor* i_blockDescriptor) {
 	BlockDescriptor* head = uninitializedBlocksList;
 	i_blockDescriptor->next = nullptr;
 	i_blockDescriptor->base = nullptr;
 	i_blockDescriptor->size = 0;
-	head->next = i_blockDescriptor;
+	i_blockDescriptor->userSize = 0;
+	uninitializedBlocksList = i_blockDescriptor;
+	uninitializedBlocksList->next = head;
 }
 
 HeapManager::HeapManager() {
-	BLOCK = _aligned_malloc(BLOCK_SIZE+ sizeof(BlockDescriptor)*NUMBER_OF_BLOCKDESCRIPTORS, 4);
+	BLOCK = _aligned_malloc(BLOCK_SIZE + sizeof(BlockDescriptor)*NUMBER_OF_BLOCKDESCRIPTORS, 4);
 	initializeBlockDescriptors();
 	// Initialize free blocks list
 	freeBlocksList = getUninitializedBlock();
 	freeBlocksList->next = nullptr;
 	freeBlocksList->base = BLOCK;
 	freeBlocksList->size = BLOCK_SIZE;
-	availableBlockDescriptorsCount = NUMBER_OF_BLOCKDESCRIPTORS-1;
+	availableBlockDescriptorsCount = NUMBER_OF_BLOCKDESCRIPTORS - 1;
 	assignedBlocksList = nullptr;
 }
 
@@ -62,57 +65,57 @@ void* HeapManager::padBlockAndReturnPointer(BlockDescriptor* assignedBlock, int 
 	for (int i = 0; i < GUARD_BAND_SIZE; i++) {
 		*pointer = GUARD_BAND_FILL;
 		pointer++;
-	}	
+	}
 	pointer += i_size;
 	for (int i = 0; i < GUARD_BAND_SIZE; i++) {
 		*pointer = GUARD_BAND_FILL;
 		pointer++;
-	}	
+	}
 	pointer = (char*)assignedBlock->base;
 	return pointer + GUARD_BAND_SIZE;
 }
 
 void HeapManager::putBlockInAssignedBlockList(BlockDescriptor* assignedBlock) {
 	assignedBlock->next = nullptr;
-	if (assignedBlocksList == nullptr) {
-		assignedBlocksList = assignedBlock;
-	}
-	else {
-		BlockDescriptor* head = assignedBlocksList;
-		while (head->next != nullptr) {
-			head = head->next;
-		}
-		head->next = assignedBlock;
-	}
+	BlockDescriptor* head = assignedBlocksList;
+	assignedBlocksList = assignedBlock;
+	assignedBlocksList->next = head;
 }
 
 void HeapManager::removeBlockFromFreeBlocksList(BlockDescriptor* assignedBlock) {
 	BlockDescriptor* head = freeBlocksList;
-	while (head->next != nullptr) {
-		if (head->next == assignedBlock) {
-			head->next = assignedBlock->next;			
+	while (head != nullptr) {
+		if (head == assignedBlock) {
+			freeBlocksList = head->next;
+			break;
 		}
+		if (head->next == assignedBlock) {
+			head->next = assignedBlock->next;
+			break;
+		}
+		head = head->next;
 	}
 }
 
 void* HeapManager::getPointerFromFreeBlocks(size_t i_size) {
 	void* memoryLocation = nullptr;
 	BlockDescriptor* current = freeBlocksList;
-	do {		
+	do {
 		if (current->size > i_size + GUARD_BAND_SIZE * 2) {
 			char* pointer = (char*)current->base + current->size - GUARD_BAND_SIZE * 2 - i_size;
 			pointer = *pointer % 4 == 0 ? pointer : pointer - (4 - *pointer % 4);
 			if (pointer > current->base) {
 				BlockDescriptor* assignedBlock;
 				if (pointer - (char*)current->base >= MIN_BLOCK_SIZE) {
-					assignedBlock = splitAndReturnBlock(current, pointer);					
+					assignedBlock = splitAndReturnBlock(current, pointer);
 				}
 				else {
 					assignedBlock = current;
 				}
 				memoryLocation = padBlockAndReturnPointer(assignedBlock, i_size);
-				removeBlockFromFreeBlocksList(current);				
-				putBlockInAssignedBlockList(current);
+				removeBlockFromFreeBlocksList(assignedBlock);
+				assignedBlock->userSize = i_size;
+				putBlockInAssignedBlockList(assignedBlock);
 				break;
 			}
 		}
@@ -121,10 +124,68 @@ void* HeapManager::getPointerFromFreeBlocks(size_t i_size) {
 }
 
 void * HeapManager::allocate(size_t i_size) {
-	assert((int)i_size <= BLOCK_SIZE);	
-	assert(NUMBER_OF_BLOCKDESCRIPTORS != 0);
+	assert((int)i_size <= BLOCK_SIZE);
+	assert(availableBlockDescriptorsCount != 0);
 	void* ptr;
 	ptr = getPointerFromFreeBlocks(i_size);
 	assert(ptr != nullptr);
 	return ptr;
+}
+
+void HeapManager::checkGuardBands(void* i_ptr, BlockDescriptor* assignedBlock) {
+	char* pointer = (char*)i_ptr;
+	pointer -= GUARD_BAND_SIZE;
+	int i;
+	for (i = 0; i < GUARD_BAND_SIZE; i++) {
+		assert((unsigned char)*pointer == GUARD_BAND_FILL);
+		*pointer = FREE_BLOCKS_FILL;
+		pointer++;
+	}
+	for (i = 0; i < assignedBlock->userSize; i++) {
+		*pointer = FREE_BLOCKS_FILL;
+		pointer++;
+	}
+	for (i = 0; i < GUARD_BAND_SIZE; i++) {
+		assert((unsigned char)*pointer == GUARD_BAND_FILL);
+		*pointer = FREE_BLOCKS_FILL;
+		pointer++;
+	}
+}
+
+HeapManager::BlockDescriptor* HeapManager::findBlockForPointer(void* ptr) {
+	BlockDescriptor* assignedBlock = nullptr;
+	BlockDescriptor* head = assignedBlocksList;
+	BlockDescriptor* previousHead = nullptr;
+	while (head != nullptr) {
+		if (ptr >= head->base && ptr <= (char*)head->base + head->size) {
+			assignedBlock = head;
+			// Remove block from assigned blocks list
+			if (previousHead == nullptr) {
+				assignedBlocksList = head->next;
+			}
+			else {
+				previousHead->next = head->next;
+			}
+			break;
+		}
+		else {
+			previousHead = head;
+			head = head->next;
+		}
+	}
+	return assignedBlock;
+}
+
+void HeapManager::addBlockToFreeBlocksList(BlockDescriptor* assignedBlock) {
+	assignedBlock->userSize = 0;
+	BlockDescriptor* head = freeBlocksList;
+	freeBlocksList = assignedBlock;
+	freeBlocksList->next = head;
+}
+
+void HeapManager::free(void* ptr) {
+	BlockDescriptor* assignedBlock = findBlockForPointer(ptr);
+	assert(assignedBlock != nullptr);
+	checkGuardBands(ptr, assignedBlock);
+	addBlockToFreeBlocksList(assignedBlock);
 }
