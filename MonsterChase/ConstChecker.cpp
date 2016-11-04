f6168a57bcee8193a42c63248ed756f0e7ab37b9
Bharat Gudihal <bharat.gudi@gmail.com>
2016-11-03 18:46:41 -0600

    Added few more scenarios to the const test case

--- MonsterChase/ConstChecker.cpp
+++ MonsterChase/ConstChecker.cpp
@@ -8,8 +8,14 @@ bool ConstChecker(){
 	float x = vector1.X();
 	float y = vector1.Y();
 	x = vector2.X();
-	y = vector2.Y();
+	y = vector2.Y();	
 	Vector2D sum = vector1 + vector2;
 	Vector2D diff = vector1 - vector2;
+	sum.X(5);
+	//Tested
+	//vector1(1.0f, 1.0f, 1.0f);
+	//vector1 += vector2;
+	//vector1 = vector2;
+	//vector1 = vector1+vector2;
 	return true;
 }
