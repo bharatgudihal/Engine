c65723d9ed483fc3396638ac8dbe2ee212bc9599
Bharat Gudihal <bharat.gudi@gmail.com>
2016-11-07 21:04:41 -0700

    Changed casting from C to c++ style

--- MonsterChase/Monster/MonsterController_Inl.h
+++ MonsterChase/Monster/MonsterController_Inl.h
@@ -2,4 +2,4 @@
 #include <stdlib.h>
 
 inline void MonsterController::SetActor(Engine::Actor* actor) { monster = actor; }
-inline Vector2D MonsterController::GetRandomVector() { return Vector2D((const float)(rand() % 10 - 5), (const float)(rand() % 10 - 5)); }
+inline Vector2D MonsterController::GetRandomVector() { return Vector2D(static_cast<float>(rand() % 10 - 5), static_cast<float>(rand() % 10 - 5)); }
