b009e55875dcc2f60edfebb8f4235e6a3e01acb7
u1070597 <u1070597@eae-d-156>
2017-05-04 22:16:43 -0600

    refactored collision code

--- Engine/Physics/Private/CollisionCheck.cpp
+++ Engine/Physics/Private/CollisionCheck.cpp
@@ -4,25 +4,71 @@ namespace Engine {
 	namespace Physics {
 		namespace Collision {
 
-			bool TestCollisionOfBInA(GameObject::GameObject* objectA, GameObject::GameObject* objectB, float deltaTime, float& o_latestTClose, float& o_earliestTOpen, 
-				float& o_collisionTime, Math::Vector4& o_collisionNormal) {
+			bool CheckSeparation(float VelBInA, float BCenterInA, float BExtentsXInA, float BExtentsYInA, float ExtentsA, float CenterA, float tEnd, float& o_tClose, float& o_tOpen) {
+				//Get B's extents projection
+				float BProjectionOnA = fabs(BExtentsXInA) + fabs(BExtentsYInA);
+				if (VelBInA != 0.0f) {
+					//Calculate DClose
+					float DClose = CenterA - ExtentsA - BProjectionOnA - BCenterInA;
+					//Calculate DOpen
+					float DOpen = CenterA + ExtentsA + BProjectionOnA - BCenterInA;
+					//Calculate tClose
+					o_tClose = DClose / VelBInA;
+					//Calculate tOpen
+					o_tOpen = DOpen / VelBInA;
+					if (o_tOpen < o_tClose) {
+						//Moving in opposite direction
+						float temp = o_tOpen;
+						o_tOpen = o_tClose;
+						o_tClose = temp;
+					}
+					return (o_tClose > tEnd || o_tOpen < 0.0f);
+				}
+				else {
+					return fabs(CenterA - BCenterInA) > ExtentsA + BProjectionOnA;
+				}
+			}
 
-				bool isSeparated;
+			bool TestCollisionOfBInA(GameObject::GameObject* objectA, GameObject::GameObject* objectB, float deltaTime, float& o_latestTClose, float& o_earliestTOpen,
+				Math::Vector4& o_collisionNormal) {
+
 				float tStart = 0.0f;
 				float tEnd = deltaTime;
-				Math::Opt::Matrix ATranslationMatrix = Math::Matrix::GetTranslationMatrix((*(objectA->GetActorReference()))->GetPosition());
-				Math::Opt::Matrix ARotationMatrix = Math::Matrix::GetZRotationMatrix((*(objectA->GetActorReference()))->GetRotation().Z());
+				Math::Vector3& positionA = (*(objectA->GetActorReference()))->GetPosition();
+				Math::Vector3& positionB = (*(objectB->GetActorReference()))->GetPosition();
+				float zRotationA = (*(objectA->GetActorReference()))->GetRotation().Z();
+				float zRotationB = (*(objectB->GetActorReference()))->GetRotation().Z();
+				Math::Vector3 CenterA = (*(objectA->GetActorReference()))->GetBounds().Center;
+				Math::Vector3 ExtentsA = (*(objectA->GetActorReference()))->GetBounds().Extents;
+				Math::Vector3 CenterB = (*(objectB->GetActorReference()))->GetBounds().Center;
+				Math::Vector3 ExtentsB = (*(objectB->GetActorReference()))->GetBounds().Extents;
+				//Get A's translation matrix
+				Math::Opt::Matrix ATranslationMatrix = Math::Matrix::GetTranslationMatrix(positionA);
+				//Get A's rotation matrix
+				Math::Opt::Matrix ARotationMatrix = Math::Matrix::GetZRotationMatrix(zRotationA);
+				//Get A to world matrix
 				Math::Opt::Matrix AtoWorld = ATranslationMatrix * ARotationMatrix;
+				//Get world to A matrix
 				Math::Opt::Matrix WorldToA = AtoWorld.GetStandardMatrix().GetInverse();
-				Math::Opt::Matrix BTranslationMatrix = Math::Matrix::GetTranslationMatrix((*(objectB->GetActorReference()))->GetPosition());
-				Math::Opt::Matrix BRotationMatrix = Math::Matrix::GetZRotationMatrix((*(objectB->GetActorReference()))->GetRotation().Z());
+				//Get B's translation matrix
+				Math::Opt::Matrix BTranslationMatrix = Math::Matrix::GetTranslationMatrix(positionB);
+				//Get B's rotation matrix
+				Math::Opt::Matrix BRotationMatrix = Math::Matrix::GetZRotationMatrix(zRotationB);
+				//Get B to world matrix
 				Math::Opt::Matrix BtoWorld = BTranslationMatrix * BRotationMatrix;
+				//Get world to B matrix
 				Math::Opt::Matrix WorldToB = BtoWorld.GetStandardMatrix().GetInverse();
+				//Get B to A matrix
 				Math::Opt::Matrix BtoA = WorldToA * BtoWorld;
+				//Get A to B matrix
 				Math::Opt::Matrix AtoB = WorldToB * AtoWorld;
-				Math::Vector4 BCenterInA = BtoA * Math::Vector4((*(objectB->GetActorReference()))->GetBounds().Center, 1.0f);
-				Math::Vector4 BExtentsXInA = BtoA * Math::Vector4((*(objectB->GetActorReference()))->GetBounds().Extents.X(), 0.0f, 0.0f, 0.0f);
-				Math::Vector4 BExtentsYInA = BtoA * Math::Vector4(0.0f, (*(objectB->GetActorReference()))->GetBounds().Extents.Y(), 0.0f, 0.0f);
+				//Get B's center in A
+				Math::Vector4 BCenterInA = BtoA * Math::Vector4(CenterB, 1.0f);
+				//Get B's X extents in A
+				Math::Vector4 BExtentsXInA = BtoA * Math::Vector4(ExtentsB.X(), 0.0f, 0.0f, 0.0f);
+				//Get B's Y extents in A
+				Math::Vector4 BExtentsYInA = BtoA * Math::Vector4(0.0f, ExtentsB.Y(), 0.0f, 0.0f);
+
 				Math::Vector3 VelA = Math::Vector3::ZERO;
 				Math::Vector3 VelB = Math::Vector3::ZERO;
 				if (objectA->GetPhysicsBody() != nullptr) {
@@ -31,73 +77,28 @@ namespace Engine {
 				if (objectB->GetPhysicsBody() != nullptr) {
 					VelB = objectB->GetPhysicsBody()->GetVelocity();
 				}
+				//Get relative velocity of B and A
 				Math::Vector3 VelBRelToA = VelB - VelA;
-				Math::Vector4 VelBInA = WorldToA * Math::Vector4(VelBRelToA, 0.0f);				
-				float BProjectionOnA_X = fabs(BExtentsXInA.X()) + fabs(BExtentsYInA.X());
-				float AExtents_X = (*(objectA->GetActorReference()))->GetBounds().Extents.X() + BProjectionOnA_X;
-				float ALeft = (*(objectA->GetActorReference()))->GetBounds().Center.X() - AExtents_X;
-				float ARight = (*(objectA->GetActorReference()))->GetBounds().Center.X() + AExtents_X;
-				if (VelBInA.X() != 0.0f) {
-					float DClose_X = ALeft - BCenterInA.X();
-					float DOpen_X = ARight - BCenterInA.X();
-					float tClose = DClose_X / VelBInA.X();
-					float tOpen = DOpen_X / VelBInA.X();
-					if (tOpen < tClose) {
-						//Moving in opposite direction
-						float temp = tOpen;
-						tOpen = tClose;
-						tClose = temp;
-					}						
-					o_latestTClose = tClose;
-					o_earliestTOpen = tOpen;
-					isSeparated = tClose > tEnd || tOpen < 0.0f;
-					if (!isSeparated && tClose > 0.0f && o_collisionTime > tClose) {
-						o_collisionTime = tClose;						
-						o_collisionNormal = AtoWorld * Math::Vector4(1.0f,0.0f,0.0f,0.0f);
-					}
+				//Get B's velocity in A
+				Math::Vector4 VelBInA = WorldToA * Math::Vector4(VelBRelToA, 0.0f);
+				float tcloseX = 0.0f;
+				float topenX = 0.0f;
+				float tcloseY = 0.0f;
+				float topenY = 0.0f;
+				bool isSeparatedOnY = true;
+				bool isSeparatedOnX = CheckSeparation(VelBInA.X(), BCenterInA.X(), BExtentsXInA.X(), BExtentsYInA.X(), ExtentsA.X(), CenterA.X(), tEnd, tcloseX, topenX);
+				if (!isSeparatedOnX) {
+					isSeparatedOnY = CheckSeparation(VelBInA.Y(), BCenterInA.Y(), BExtentsXInA.Y(), BExtentsYInA.Y(), ExtentsA.Y(), CenterA.Y(), tEnd, tcloseY, topenY);
 				}
-				else {
-					isSeparated = BCenterInA.X() < ALeft || BCenterInA.X() > ARight;
-					if (!isSeparated) {
-						o_collisionNormal = AtoWorld * Math::Vector4(1.0f, 0.0f, 0.0f, 0.0f);
-					}
+				o_latestTClose = tcloseX > tcloseY ? tcloseX : tcloseY;
+				o_earliestTOpen = topenX < topenY ? topenX : topenY;
+				if (o_latestTClose == tcloseX) {
+					o_collisionNormal = AtoWorld * Math::Vector4(1.0f, 0.0f, 0.0f, 0.0f);
 				}
-				if (!isSeparated) {
-					float BProjectionOnA_Y = fabs(BExtentsXInA.Y()) + fabs(BExtentsYInA.Y());
-					float AExtents_Y = (*(objectA->GetActorReference()))->GetBounds().Extents.Y() + BProjectionOnA_Y;
-					float ABottom = (*(objectA->GetActorReference()))->GetBounds().Center.Y() - AExtents_Y;
-					float ATop = (*(objectA->GetActorReference()))->GetBounds().Center.Y() + AExtents_Y;
-					if (VelBInA.Y() != 0.0f) {
-						float DClose_Y = ABottom - BCenterInA.Y();
-						float DOpen_Y = ATop - BCenterInA.Y();
-						float tClose = DClose_Y / VelBInA.Y();
-						float tOpen = DOpen_Y / VelBInA.Y();
-						if (tOpen < tClose) {
-							//Moving in opposite direction
-							float temp = tOpen;
-							tOpen = tClose;
-							tClose = temp;
-						}
-						if (o_latestTClose < tClose) {
-							o_latestTClose = tClose;
-						}
-						if (o_earliestTOpen > tOpen) {
-							o_earliestTOpen = tOpen;
-						}
-						isSeparated = tClose > tEnd || tOpen < 0.0f;
-						if (!isSeparated && tClose > 0.0f && o_collisionTime > tClose) {
-							o_collisionTime = tClose;
-							o_collisionNormal = AtoWorld * Math::Vector4(0.0f, 1.0f, 0.0f, 0.0f);
-						}
-					}
-					else {
-						isSeparated = BCenterInA.Y() < ABottom || BCenterInA.Y() > ATop;
-						if (!isSeparated) {
-							o_collisionNormal = AtoWorld * Math::Vector4(0.0f, 1.0f, 0.0f, 0.0f);
-						}
-					}
+				else if (o_latestTClose == tcloseY) {
+					o_collisionNormal = AtoWorld * Math::Vector4(0.0f, 1.0f, 0.0f, 0.0f);
 				}
-				return !isSeparated;
+				return !isSeparatedOnX && !isSeparatedOnY;
 			}
 
 			bool CheckCollision(GameObject::GameObject* objectA, GameObject::GameObject* objectB, float deltaTime, float& o_collisionTime, Math::Vector4& o_collisionNormal) {
@@ -106,13 +107,24 @@ namespace Engine {
 				float earliestTOpenAB = 0.0f;
 				float latestTCloseBA = 0.0f;
 				float earliestTOpenBA = 0.0f;
-				result = TestCollisionOfBInA(objectA, objectB, deltaTime, latestTCloseAB, earliestTOpenAB, o_collisionTime, o_collisionNormal);
+				Math::Vector4 collisionNormalAB;
+				Math::Vector4 collisionNormalBA;
+				result = TestCollisionOfBInA(objectA, objectB, deltaTime, latestTCloseAB, earliestTOpenAB, collisionNormalAB);
 				if (result) {
-					result = TestCollisionOfBInA(objectB, objectA, deltaTime, latestTCloseBA, earliestTOpenBA, o_collisionTime, o_collisionNormal);
+					result = TestCollisionOfBInA(objectB, objectA, deltaTime, latestTCloseBA, earliestTOpenBA, collisionNormalBA);
 					if (result) {
 						float latestClose = latestTCloseAB > latestTCloseBA ? latestTCloseAB : latestTCloseBA;
 						float earliestOpen = earliestTOpenAB > earliestTOpenBA ? earliestTOpenAB : earliestTOpenBA;
-						result = latestClose < earliestOpen;						
+						if (latestClose == latestTCloseAB) {
+							o_collisionNormal = collisionNormalAB;
+						}
+						else if (latestClose == latestTCloseBA) {
+							o_collisionNormal = collisionNormalBA;
+						}
+						result = latestClose < earliestOpen;
+						if (result) {
+							o_collisionTime = latestClose;
+						}
 					}
 				}
 				return result;
