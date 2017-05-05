4ed7a249f1496eb2020f0d0f1365938828ac2c33
u1070597 <u1070597@eae-d-156>
2017-05-04 23:03:28 -0600

    modified matrix opt class

--- Engine/Math/Matrix_Opt.h
+++ Engine/Math/Matrix_Opt.h
@@ -14,7 +14,7 @@ namespace Engine {
 					const float i_a31 = 0.0f, const float i_a32 = 0.0f, const float i_a33 = 0.0f, const float i_a34 = 0.0f,
 					const float i_a41 = 0.0f, const float i_a42 = 0.0f, const float i_a43 = 0.0f, const float i_a44 = 0.0f);
 				Engine::Math::Matrix GetStandardMatrix() const;
-				__m128 row1, row2, row3, row4;
+				__m128 rows[4];
 				struct {
 					float a11, a12, a13, a14,
 						a21, a22, a23, a24,
